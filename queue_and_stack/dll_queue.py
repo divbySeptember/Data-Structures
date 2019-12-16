6b4aec457bfa6473eb320e2e31a6e21b7ed2aee1
divbyseptember <beatsbyseptember@gmail.com>
2019-12-16 18:09:56 -0500

    Completed Day #1 ,MVP

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,20 +3,27 @@ sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
-
-
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # self.storage.add_to_tail
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # return self.storage.remove_from_head()
+        if self.size == 0:
+            return
+        else:
+            last = self.storage.remove_from_head()
+            self.size -= 1
+            return last
 
     def len(self):
-        pass
+        return self.size
+        # return self.storage.length
 
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,41 @@
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import  DoublyLinkedList
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # self.storage.add_to_head(value)
+        # self.size += 1
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        # if self.size == 0:
+        #     return
+        # else:
+        #     last = self.storage.remove_from_head()
+        #     self.size -= 1
+        #     return last
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        # return self.size
+        return self.storage.__len__()
+
+
+stack = Stack()
+print('Should be 0')
+print(stack.len())
+stack.push(4)
+print('Should be 1')
+print(stack.len())
+stack.push(6)
+print('Should be 2')
+print(stack.len())
+print('Should be 6')
+print(stack.pop())
